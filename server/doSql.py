eeb4fd61acae51a8af0567c3a8958e3170242be0
hzshang <hzshang15@gmail.com>
2017-04-13 21:39:08 +0800

    update

--- server/doSql.py
+++ server/doSql.py
@@ -70,19 +70,140 @@ def askForCourses(recvData):#学生查看可选课程
 	array=[dict(cid=i[0],cname=i[1],current=i[2],max=i[3],ctime=i[4],tname=i[5],rid=i[6]) for i in piece]
 	return {"total":len(cur.fetchall()),"courses":array}
 
+def initSchedule(user):
+	cur=gl.conn.cursor()
+	time=["0000000000" for i in range(0,7)]
+	sql="select ctime from sc,course where sc.cid=course.cid and sc.sid=\"%s\""%recvData["user"]
+	cur.execute(sql)
+	for i in cur.fetchall():
+		ctime=i[0]
+		j=0
+		while j<int(ctime[2]):
+			time[int(ctime[0])][int(ctime[1])+j]='1'
+			j=j+1
+	return time
+
+def checkTimeIsOK(ctime,time):#检查课程时间是否冲突
+	j=0
+	ok=True
+	while j<int(ctime[2]):
+		if time[int(ctime[0])][int(ctime[1])+j]!='0':
+			ok=False
+			break
+	return ok
 
 def selectCourse(recvData):#学生选课
-	pass
+	time=initSchedule(recvData["user"])
+
+	cur=gl.conn.cursor()
+	sql="select ctime from course where cid=\"%s\""%recvData["cid"]
+	cur.execute(sql)
+	ctime=cur.fetchall()[0][0]
+
+	if not checkTimeIsOK(ctime,time):
+		return {"state":False}
+	try:
+		sql="insert into sc values(\"%s\",\"%s\")"%(recvData["user"],recvData["cid"])
+		cur.execute(sql)
+		cur.commit()
+		ret={"state":True}
+	except Exception as e:
+		print(e)
+		ret={"state":False}
+	return ret
+
 def quitCourse(recvData):#学生退课
-	pass
+	cur=gl.conn.cursor()
+	try:
+		sql="delete from sc where sid=\"%s\" and cid=\"%s\""%(recvData["user"],recvData["cid"])
+		cur.execute(sql)
+		cur.commit()
+		ret={"state":True}
+	except Exception as e:
+		print(e)
+		ret={"state":False}
+	return ret
+
+def findMaxCourseID(tid):#查找最大的课程id
+	cur=gl.conn.cursor()
+	sql='select max(cid) from course where tid="%s"'%tid
+	cur.execute()
+	if len(cur.fetchall())==0:
+		cid=tid+"00"
+	else:
+		cid=int(cur.fetchall()[0][0])+1
+	return cid
+
+def addCtimeToRoom(rid,ctime):#将课程时间添加到某教室时间表
+	dic={'1':'monday','2':'tuesday','3':'wednesday','4':'thursday','5':'friday','6':'satarday','7':'sunday'}
+	cur=gl.conn.cursor()
+	sql="select %s from room where rid='%s'"%(dic[ctime[0]],rid)
+	cur.execute(sql)
+	time=cur.fetchall()[0][0]
+	j=0
+	while j<int(ctime[2]):
+		time[int(ctime[1])+j]='1'
+		j++
+	sql="update room set %s=%s where rid='%s'"%(dic[ctime[0]],time,rid)
+	cur.execute(sql)
+	cur.commit()
+	return
+
+
 def addCourse(recvData):#老师加课
-	pass
+	cur=gl.conn.cursor()
+	sql="select * from room where rid=\"%s\""%recvData["rid"]
+	cur.execute(sql)
+	if not checkTimeIsOK(recvData["ctime"],cur.fetchall()[0][1:]):
+		return {"state":False}
+	cid=findMaxCourseID(recvData["tid"])
+	sql='insert into course values("%s","%s","%s","%s","%s","%s","%s")'\
+	%(cid,recvData["cname"],0,recvData["max"],recvData["user"],recvData["ctime"],recvData["rid"])
+	cur.execute(sql)
+	cur.commit()
+	addCtimeToRoom(recvData["rid"],recvData["ctime"])
+	return {"state":True,"cid":cid}
+
+def delCtimeFromRoom(rid,ctime):#将课程时间从某教室时间表移除
+	dic={'1':'monday','2':'tuesday','3':'wednesday','4':'thursday','5':'friday','6':'satarday','7':'sunday'}
+	cur=gl.conn.cursor()
+	sql="select %s from room where rid='%s'"%(dic[ctime[0]],rid)
+	cur.execute(sql)
+	time=cur.fetchall()[0][0]
+	j=0
+	while j<int(ctime[2]):
+		time[int(ctime[1])+j]='0'
+		j++
+	sql="update room set %s=%s where rid='%s'"%(dic[ctime[0]],time,rid)
+	cur.execute(sql)
+	cur.commit()
+	return
+
 def delCourse(recvData):#老师删课
-	pass
+#先删除sc,再删除c,再修改room
+	cur=gl.conn.cursor()
+	try:
+		sql="delete from sc where cid='%s'"%recvData["cid"]
+		cur.execute(sql)
+		sql="select rid,ctime from course where cid='%s'"%recvData["cid"]
+		cur.execute(sql)
+		data=cur.fetchall()[0]
+		sql="delete from course where cid='%s'"%recvData["cid"]
+		cur.execute(sql)
+		cur.commit()
+		delCtimeFromRoom(*data)
+	except Exception as e:
+		print(e)
+		cur.rollback()
+		return {"state":False}
+	return {"state":True}
+
 def seeMyCourse(recvData):#老师查看所有开设的课程
 	pass
 def seeMyCourseStudent(recvData):#老师查看选择某课的学生
 	pass
+
+
 switch={
 	1:signin,
 	2:signup,
